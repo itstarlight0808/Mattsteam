41909c7005675f7add1efeb14f3d3ea04b7accec
BSt@r0808h <it_starlight0808@outlook.com>
2021-05-27 13:03:41 +0900

    updating the UI

--- hivetree.php
+++ hivetree.php
@@ -1,3 +1,10 @@
+<?php
+	if(isset($_GET['seid']) && $_GET['seid']>0){
+		$seid = $_GET['seid'];
+	}
+	else
+		$seid = 1;
+?>
 <head>
 	<script src="https://d3js.org/d3.v3.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" 
@@ -5,12 +12,18 @@
             crossorigin="anonymous"></script>
 	<script src="./hivetree.js" type="text/javascript"></script>
 	<link rel="stylesheet" href="./custom.css">
-	<style type="text/css">
-	</style>
+	<script>
+		var seid = <?=$seid?>;
+	</script>
 </head>
 <body>
   	<div>
   		<?php include("navigation.php"); ?>
+		<div class="controlDiv">
+			<button id="backBtn" class="btn blue-btn"">Back</button>
+			<button id="ExpandAllBtn" class="btn blue-btn">Expand All</button>
+			<button id="CollapseAllBtn" class="btn blue-btn">Collapse All</button>
+		</div>
  	</div>
 	<div id="container">
 	</div>
